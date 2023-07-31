388e36f8994ce249818f8d4a1a00bd88918e144d
DJDhana <dhanasekharmarolix@gmail.com>
2023-07-31 09:52:10 +0530

    this my assignment

---
+++ assignment1/firstassignment.py
@@ -0,0 +1,96 @@
+# print positive number or negitive 
+#a = int (input(" enter a number:"))
+#if a>=0:
+    #print("the given number is positive")
+#else:
+
+
+   # print("the given number is negitive")
+# take two number which one is greater 
+#a = int (input("enter a number:"))
+#b = int (input("enter a number:"))
+#if a > b:
+    #print("a is greater than b ")
+#elif b > a:
+    #print("b is grather than a")
+
+
+# take a string repat it 5 times
+
+#for i in range(5):
+    #print("DHANA SEKHAR")
+
+
+# take a string repat it 5 times
+
+#i=0
+#while i<=5:
+    #print("DHANASEKHAR")
+    #i+=1
+
+# take a string print a half string  
+
+#a = "Dhanasekhar"
+#b = len(a)//2
+#print(a[:b])
+
+# take a string print a half string  
+
+#a = "Dhanasekhar"
+#b = a[len(a)//2:]
+#print(b)
+
+# check the student pass or fail
+
+#a = int (input("enter a marks:"))
+#if a <=34:
+    #print("fail")
+#elif a >=35:
+    #print("pass")
+
+# check the eligible for vote or not
+
+#a = int(input("enter a age:"))
+#if a >=18:
+  #  print("eligible")
+#else:
+    #print("not eligible")
+
+# check the length and breath is equal to square or not
+
+#a = input("enter a length:")
+#b = input("enter a breath:")
+#if  a == b:
+    #print ("square")
+#else:
+    #print("not square")
+
+# print even numbers
+
+# for i in range (0,101):
+    #if i % 2==0:
+        #print(i)
+
+# print odd numbers
+
+#for i in range(0,101):
+    #if i % 2 !=0:
+        #print(i)
+
+#pattern programs
+
+#num = int(input("enter a value:"))
+#for i in range(1,num+1):
+    #for j in range(1,i+1):
+        #print("*",end=" ")
+    #print( )
+
+#pattern programs
+
+#num = int(input("enter a value:"))
+#for i in range(0,num):
+    #for j in range (0,num-i-1):
+        #print(end=" ")
+    #for k in range (0,i+1):
+        #print("*",end="")
+    #print(  )
